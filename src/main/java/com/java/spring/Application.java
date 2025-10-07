a42ffde61dd360dfbfbee037870663be196eb1f7
Yeon6v6 <merleawe012@gmail.com>
2025-10-07 17:30:06 +0900

    간단한 FrontController 구현

--- src/main/java/com/java/spring/Application.java
+++ src/main/java/com/java/spring/Application.java
@@ -1,11 +1,14 @@
 package com.java.spring;
 
+import com.java.spring.controller.HelloController;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import java.io.IOException;
 
@@ -13,16 +16,28 @@ public class Application {
     public static void main(String[] args) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
         WebServer webServer = factory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            HelloController helloController = new HelloController();
+
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp)
                         throws ServletException, IOException {
-                    resp.setStatus(200);
-                    resp.setContentType("text/plain");
-                    // 요청 파라미터를 읽어 동적으로 응답하기
-                    resp.getWriter().println("Hello " + req.getParameter("name"));
+
+                    // 공통 처리 (예: 인증, 로깅 등)
+                    // path가 /hello이고 method가 GET인 요청 처리(=> URL과 Controller 맵핑)
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals("GET")) {
+                        // 요청 파라미터 바인딩
+                        String name = req.getParameter("name");
+                        String returnValue = helloController.hello(name);
+
+                        resp.setStatus(HttpStatus.OK.value());
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        resp.getWriter().println(returnValue);
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+            }).addMapping("/*");
         });
         webServer.start();
     }
