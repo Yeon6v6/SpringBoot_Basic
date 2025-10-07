7310937074db6bed0e76279133192d46c98d2e0f
Yeon6v6 <merleawe012@gmail.com>
2025-10-07 16:54:21 +0900

    내장 톰캣 직접 띄우기

--- src/main/java/com/java/spring/Application.java
+++ src/main/java/com/java/spring/Application.java
@@ -1,11 +1,13 @@
 package com.java.spring;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
-@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
+        // 실제 웹 서버 인스턴스 생성
+        WebServer webServer = factory.getWebServer();
+        webServer.start(); // 톰캣 서블릿 컨테이너 실행
     }
 }
