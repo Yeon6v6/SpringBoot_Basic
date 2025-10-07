a735ec9e2b188e35878aa71845bf94a0412a3bef
Yeon6v6 <merleawe012@gmail.com>
2025-10-07 16:55:38 +0900

    ServletContainer에 Servlet 등록하기

--- src/main/java/com/java/spring/Application.java
+++ src/main/java/com/java/spring/Application.java
@@ -1,13 +1,29 @@
 package com.java.spring;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
+
+import java.io.IOException;
 
 public class Application {
     public static void main(String[] args) {
         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        // 실제 웹 서버 인스턴스 생성
-        WebServer webServer = factory.getWebServer();
-        webServer.start(); // 톰캣 서블릿 컨테이너 실행
+        // 컨테이너에 서블릿 등록
+        WebServer webServer = factory.getWebServer(servletContext -> {
+            servletContext.addServlet("hello", new HttpServlet() {
+                @Override
+                protected void service(HttpServletRequest req, HttpServletResponse resp)
+                        throws ServletException, IOException {
+                    resp.setStatus(200);
+                    resp.setContentType("text/plain");
+                    resp.getWriter().println("Hello Servlet");
+                }
+            }).addMapping("/hello");
+        });
+        webServer.start();
     }
 }
